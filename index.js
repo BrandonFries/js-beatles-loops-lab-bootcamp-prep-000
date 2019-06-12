e8cd8f675365aab0b427bd9f1a1855b28d75e671
Brandon Fries <brandonfries1993@gmail.com>
2019-06-12 19:19:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ function theBeatlesPlay(musicians, instruments) {
  }
 function johnLennonFacts(array) {
   let newArray= [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
+    "He was the last Beatle to learn to drive!!!",
+    "He was never a vegetarian!!!",
+    "He was a choir boy and boy scout!!!",
+    "He hated the sound of his own voice!!!"
   ]
   let i = 0
   while(newArray.length < array.length) {
-    newArray.push(array[i]+ "!!!")
-    i++
   }
   return newArray
 }
